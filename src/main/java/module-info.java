616326d490f32765b3fa6851a1e0c6b85126d3fa
zc <zengchao@hvgroup.com.cn>
2024-01-15 11:36:08 +0800

    JavaFX入门程序

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ module com.zc.javafxdemo {
 
 
     opens com.zc.javafxdemo to javafx.fxml;
+    exports com.zc.javafxdemo;
+    exports com.zc.javafxdemo.demo1;
+    opens com.zc.javafxdemo.demo1 to javafx.fxml;
 }
