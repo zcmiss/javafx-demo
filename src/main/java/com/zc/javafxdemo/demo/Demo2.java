f5f14131541efe2bf4f93374cd52291f79d2756f
zc <zengchao@hvgroup.com.cn>
2024-01-15 11:51:43 +0800

    启动图形界面的三种方式

---
+++ src/main/java/com/zc/javafxdemo/demo/Demo2.java
@@ -0,0 +1,19 @@
+package com.zc.javafxdemo.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * demo2
+ * 启动图形界面的三种方式
+ *
+ * @author zc
+ * @date 2024/01/15
+ */
+public class Demo2 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Launch类");
+        stage.show();
+    }
+}
